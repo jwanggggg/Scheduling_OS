a77a80c9a794d4e0d429d2a58cf560fd1943fa31
jwanggggg <30448951+jwanggggg@users.noreply.github.com>
2019-03-06 11:32:31 -0500

    Update Scheduling.java

--- Scheduling.java
+++ Scheduling.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Scheduling {
 	
 	public static void main(String[] args) throws IOException {
-		File input = new File("src/InputLab2/input-5");
+		File input = new File("src/InputLab2/input-3");
 		File randomNumbers = new File("src/InputLab2/random-numbers.txt");
 		Scanner inputScanner = new Scanner(input);
 		Scanner randomScanner = new Scanner(randomNumbers);
@@ -41,6 +41,7 @@ public class Scheduling {
 			currentProcess.ioTime = ioTime;
 			currentProcess.state = "unstarted";
 			currentProcess.setABCIO();
+			currentProcess.index = (i-4)/4;
 			
 			processList.add(currentProcess);
 		}
@@ -49,121 +50,338 @@ public class Scheduling {
 		
 		Collections.sort(processList);
 		
-		FCFS(processList, randomScanner);
-//		uniprogramming(processList, randomScanner);
+//		FCFS(processList, randomScanner); // First Come First Serve
+		uniprogramming(processList, randomScanner); // Uniprogramming
+//		SJF(processList, randomScanner); // Shortest Job First
+//		roundRobin(processList, randomScanner); // Round Robin
 		
-//		printInfo(processList);
 		inputScanner.close();
 	}
 	
-	public static void FCFS(List<Process> processList, Scanner randomScanner) {
+	public static void roundRobin(List<Process> processList, Scanner randomScanner) {
+		Process.resetStatic();
+		
+		Process runningProcess = null;
 		Queue<Process> readyQueue = new LinkedList<>();
-		LinkedList<Process> blockedQueue = new LinkedList<>();
+
+		List<Process> blockedList = new ArrayList<>();
+		List<Process> terminatedList = new ArrayList<>();
 		
 		System.out.println("Before cycle\t" + Process.cycle++ + ": " + processList);
 		
-		int totalTime = 0;
-		for (Process process : processList)
-			totalTime += process.cpuTime;
-		
-		// Check for ready processes
-		for (Process process : processList) {
-			if (process.state.equals("unstarted") && process.arrivalTime <= Process.cycle) {
-				process.state = "ready";
-				readyQueue.add(process);
-			}		
-		}
-		
-		while (Process.cycle < totalTime) {
-			Process blockedTop = blockedQueue.poll();
-			Process readyTop = readyQueue.poll();
-			
-			// Instead of blockedQueue need a for loop here that goes over and decrements accordingly
-			
-			if (blockedTop != null) {
-				int rand = randomScanner.nextInt();
-				int blockTime = (rand % blockedTop.ioTime) + 1;
-				blockedTop.blockTime = blockTime;
-				System.out.println("Find I/O burst when blocking a process " + rand);
+		while (terminatedList.size() < processList.size()) {			
+			// Check for ready processes
+			for (Process process : processList) {
+				if (process.state.equals("unstarted") && process.arrivalTime < Process.cycle) {
+					process.state = "ready";
+					readyQueue.add(process);
+				}
 			}
 			
-			if (readyTop != null) {
-				readyTop.state = "running";
-				int rand = randomScanner.nextInt();
-				int runTime = (rand % readyTop.cpuBurstTime) + 1;
-				readyTop.cpuRunTime = runTime;
-				System.out.println("Find burst when choosing ready process to run " + rand);
-			}
-			
-			
-			// Continue iterating until the ready and blocked times reach 0.
-			while ((readyTop != null && readyTop.cpuTime > 0 && readyTop.cpuRunTime > 0) || 
-					(blockedTop != null && blockedTop.blockTime > 0)) {
-				
-				System.out.println("Before cycle\t" + Process.cycle + ": " + processList);
-				Process.cycle++;
-				
-				for (Process process : processList) {
+			if (runningProcess == null) {
+				runningProcess = readyQueue.poll();
+				if (runningProcess != null) {
+					runningProcess.state = "running";
+					
+					// If using quantum remainder, set cpuRunTime to the remainder
+					
+					if (runningProcess.useQuantumRemainder) {
+						runningProcess.cpuRunTime = runningProcess.quantumRemainder;
+						
+						int quantum = 0;
+						if (runningProcess.cpuRunTime > 2) {
+							quantum = 2;
+							runningProcess.quantumRemainder = runningProcess.cpuRunTime - quantum;
+							runningProcess.cpuRunTime = quantum;
+							runningProcess.quantum = quantum;
+						} 
+						else {
+							runningProcess.quantum = runningProcess.cpuRunTime;
+							runningProcess.quantumRemainder = 0;
+						}
+					} else {
+						int rand = randomScanner.nextInt();
+						int runTime = (rand % runningProcess.cpuBurstTime) + 1;
+						System.out.println("Find burst when choosing ready process to run " + rand);
+						
+						runningProcess.cpuRunTime = runTime;
+						
+						int quantum = 0;
+						if (runningProcess.cpuRunTime > 2) {
+							quantum = 2;
+							runningProcess.quantumRemainder = runTime - quantum;
+							runningProcess.cpuRunTime = quantum;
+							runningProcess.quantum = quantum;
+							
+						} 
+						else {
+							runningProcess.quantum = runningProcess.cpuRunTime;
+							runningProcess.quantumRemainder = 0;
+						}
+					}
 					
 				}
-				
-				if (readyTop != null && readyTop.cpuTime > 0 && readyTop.cpuRunTime > 0) {
-					readyTop.cpuRunTime--;
-					readyTop.cpuTime--;
-					if (readyTop.cpuRunTime == 0) {
-						if (blockedTop != null && blockedTop.blockTime > 0) {
-							System.out.println("Adding blocked top with blockTime " + --blockedTop.blockTime);
-							blockedQueue.addFirst(blockedTop);
-						}
-						System.out.println(blockedQueue);
-						break;
-					}
-				}
-				// Don't decrement just blockedTop since there can be multiple blocked processes.
-				// Loop through a list of blocked processes and decrement them together.
-				if (blockedTop != null && blockedTop.blockTime > 0) {
-					blockedTop.blockTime--;
-					if (blockedTop.blockTime == 0) {
-						blockedTop.state = "ready";
-						readyQueue.add(blockedTop);
-					}
-				}
-				
-			} // end while loop
-			
-			// Both ready and blocked process have reached 0 at this point.
-			
-			// CPU time for ready process has reached 0, can set as terminated
-			if (readyTop != null && readyTop.cpuTime <= 0) {
-				readyTop.state = "terminated";
 			}
 			
-			// For the blocked check, check if there's something in the queue, then set to running/ready
-			// accordingly
+			System.out.println("Before cycle\t" + Process.cycle + ": " + processList);
 			
-			// Otherwise put it into the blocked queue.
-			else if (readyTop != null && readyTop.cpuRunTime <= 0) {
-				System.out.println("adding blocked top " + readyTop);
-				readyTop.state = "blocked";
-				blockedQueue.addFirst(readyTop);
+			if (runningProcess != null) {
+				runningProcess.cpuTime--;
+				runningProcess.cpuRunTime--;
+				runningProcess.quantum--;
+			}
+			Process.cycle++;
+			
+			// Now check blocked processes and decrement @ each iteration
+			
+			List<Process> addReadyList = new ArrayList<>();
+			Process[] blockedListArray = blockedList.toArray(new Process[0]);
+			
+			for (int i = 0; i < blockedListArray.length; i++) {
+				Process blockedProcess = blockedListArray[i];
+				blockedProcess.blockTime--;
+				
+				if (blockedProcess.blockTime == 0) {
+					// Queue up the process and remove it from the blocked list
+					blockedProcess.state = "ready";
+					addReadyList.add(blockedProcess);
+					blockedList.remove(blockedProcess);
+				}
 			}
 			
-			// Blocked time for blocked process has reached 0, can re-add to the readyQueue.
+			if (runningProcess != null) {
+				// Terminated
+				if (runningProcess.cpuTime == 0) {
+					runningProcess.state = "terminated";
+					terminatedList.add(runningProcess);
+					runningProcess = null;
+				}
+				
+				// Quantum has ticked down, re-queue the process 
+				else if (runningProcess.cpuRunTime == 0 && runningProcess.quantum == 0 && runningProcess.quantumRemainder > 0) {
+					runningProcess.quantum = runningProcess.quantumRemainder;
+					runningProcess.state = "ready";
+					runningProcess.useQuantumRemainder = true;
+					addReadyList.add(runningProcess);
+					runningProcess = null;
+				}
+				
+				// Process has blocked
+				else if (runningProcess.cpuRunTime == 0 && runningProcess.quantum == 0 && runningProcess.quantumRemainder <= 0) {
+					int rand = randomScanner.nextInt();
+					int blockTime = (rand % runningProcess.ioTime) + 1;
+					runningProcess.blockTime = blockTime;
+					runningProcess.state = "blocked";
+					runningProcess.useQuantumRemainder = false;
+					blockedList.add(runningProcess);
+					System.out.println("Find I/O burst when blocking a process " + rand);
+					runningProcess = null;
+				}
+				
+			}
 			
-			if (Process.cycle >= 15)
-				System.exit(0);
-		}
+			// Add all processes that were blocked but are now to be added to ready queue, sorting
+			// by index to break any ties that arise
+			Comparator_ArrivalTime indexComparator = new Comparator_ArrivalTime();
+			Collections.sort(addReadyList, indexComparator);
+			for (Process readyProcess : addReadyList) {
+				readyQueue.add(readyProcess);
+			}
 			
+		} // end while
+	}
+	
+	public static void SJF(List<Process> processList, Scanner randomScanner) {
+		Process.resetStatic();
+		// Use a priority queue that dequeues based on the shortest time remaining.
 		
+		Process runningProcess = null;
+		Queue<Process> readyQueue = new PriorityQueue<Process>(new Comparator<Process>() {
+
+			@Override
+			public int compare(Process process1, Process process2) {
+				if (process1.cpuTime > process2.cpuTime)
+					return 1;
+				else if (process1.cpuTime < process2.cpuTime)
+					return -1;
+				return 0;
+			}
+			
+		});
 		
-		System.out.println(readyQueue);
+		List<Process> blockedList = new ArrayList<>();
+		List<Process> terminatedList = new ArrayList<>();
+		
+		System.out.println("Before cycle\t" + Process.cycle++ + ": " + processList);
+		
+		while (terminatedList.size() < processList.size()) {
+			// Check for ready processes
+			for (Process process : processList) {
+				if (process.state.equals("unstarted") && process.arrivalTime < Process.cycle) {
+					process.state = "ready";
+					readyQueue.add(process);
+				}
+			}
+			
+			if (runningProcess == null) {
+				runningProcess = readyQueue.poll();
+				if (runningProcess != null) {
+					runningProcess.state = "running";
+					int rand = randomScanner.nextInt();
+					int runTime = (rand % runningProcess.cpuBurstTime) + 1;
+					runningProcess.cpuRunTime = runTime;
+					System.out.println("Find burst when choosing ready process to run " + rand);
+				}
+			}
+			
+			System.out.println("Before cycle\t" + Process.cycle + ": " + processList);
+			
+			if (runningProcess != null) {
+				runningProcess.cpuTime--;
+				runningProcess.cpuRunTime--;
+			}
+			Process.cycle++;
+			
+			// Now check blocked processes and decrement @ each iteration
+			
+			List<Process> addReadyList = new ArrayList<>();
+			Process[] blockedListArray = blockedList.toArray(new Process[0]);
+			
+			for (int i = 0; i < blockedListArray.length; i++) {
+				Process blockedProcess = blockedListArray[i];
+				blockedProcess.blockTime--;
+				
+				if (blockedProcess.blockTime == 0) {
+					// Queue up the process and remove it from the blocked list
+					blockedProcess.state = "ready";
+					addReadyList.add(blockedProcess);
+					blockedList.remove(blockedProcess);
+				}
+			}
+			
+			// Add all processes that were blocked but are now to be added to ready queue, sorting
+			// by index to break any ties that arise
+			Comparator_Index indexComparator = new Comparator_Index();
+			Collections.sort(addReadyList, indexComparator);
+			for (Process readyProcess : addReadyList) {
+				readyQueue.add(readyProcess);
+			}
+			
+			if (runningProcess != null) {
+				// Terminated
+				if (runningProcess.cpuTime == 0) {
+					runningProcess.state = "terminated";
+					terminatedList.add(runningProcess);
+					runningProcess = null;
+				}
+				
+				else if (runningProcess.cpuRunTime <= 0) {
+					int rand = randomScanner.nextInt();
+					int blockTime = (rand % runningProcess.ioTime) + 1;
+					runningProcess.blockTime = blockTime;
+					runningProcess.state = "blocked";
+					blockedList.add(runningProcess);
+					System.out.println("Find I/O burst when blocking a process " + rand);
+					runningProcess = null;
+				}
+				
+			}
+ 
+		} // end while
+	}
+	
+	public static void FCFS(List<Process> processList, Scanner randomScanner) {
+		Process.resetStatic();
+		
+		Process runningProcess = null;
+		Queue<Process> readyQueue = new LinkedList<>();
+		List<Process> blockedList = new ArrayList<>();
+		List<Process> terminatedList = new ArrayList<>();
+		
+		System.out.println("Before cycle\t" + Process.cycle++ + ": " + processList);
+		
+		while (terminatedList.size() < processList.size()) {
+			// Check for ready processes
+			for (Process process : processList) {
+				if (process.state.equals("unstarted") && process.arrivalTime < Process.cycle) {
+					process.state = "ready";
+					readyQueue.add(process);
+				}
+			}
+			
+			if (runningProcess == null) {
+				runningProcess = readyQueue.poll();
+				if (runningProcess != null) {
+					runningProcess.state = "running";
+					int rand = randomScanner.nextInt();
+					int runTime = (rand % runningProcess.cpuBurstTime) + 1;
+					runningProcess.cpuRunTime = runTime;
+					System.out.println("Find burst when choosing ready process to run " + rand);
+				}
+			}
+			
+			System.out.println("Before cycle\t" + Process.cycle + ": " + processList);
+			
+			if (runningProcess != null) {
+				runningProcess.cpuTime--;
+				runningProcess.cpuRunTime--;
+			}
+			Process.cycle++;
+			
+			// Now check blocked processes and decrement @ each iteration
+			
+			List<Process> addReadyList = new ArrayList<>();
+			Process[] blockedListArray = blockedList.toArray(new Process[0]);
+			
+			for (int i = 0; i < blockedListArray.length; i++) {
+				Process blockedProcess = blockedListArray[i];
+				blockedProcess.blockTime--;
+				
+				if (blockedProcess.blockTime == 0) {
+					// Queue up the process and remove it from the blocked list
+					blockedProcess.state = "ready";
+					addReadyList.add(blockedProcess);
+					blockedList.remove(blockedProcess);
+				}
+			}
+			
+			// Add all processes that were blocked but are now to be added to ready queue, sorting
+			// by index to break any ties that arise
+			Comparator_Index indexComparator = new Comparator_Index();
+			Collections.sort(addReadyList, indexComparator);
+			for (Process readyProcess : addReadyList) {
+				readyQueue.add(readyProcess);
+			}
+			
+			if (runningProcess != null) {
+				// Terminated
+				if (runningProcess.cpuTime == 0) {
+					runningProcess.state = "terminated";
+					terminatedList.add(runningProcess);
+					runningProcess = null;
+				}
+				
+				else if (runningProcess.cpuRunTime <= 0) {
+					int rand = randomScanner.nextInt();
+					int blockTime = (rand % runningProcess.ioTime) + 1;
+					runningProcess.blockTime = blockTime;
+					runningProcess.state = "blocked";
+					blockedList.add(runningProcess);
+					System.out.println("Find I/O burst when blocking a process " + rand);
+					runningProcess = null;
+				}
+				
+			}
+ 
+		} // end while
 		
 	}
 	
 	public static void uniprogramming(List<Process> processList, Scanner randomScanner) {
+		Process.resetStatic();
+		
 		// Uniprogramming
 		for (Process process : processList) {
-			
 			while (Process.cycle < process.arrivalTime) {
 				Process.cycle++;
 			}
@@ -193,13 +411,12 @@ public class Scheduling {
 			// RUNNING:
 			
 			while (process.state.equals("running")) {
-				System.out.println(process.cpuTime);
 				int rand = randomScanner.nextInt();
 				int runTime = (rand % process.cpuBurstTime) + 1;
 				
 				process.cpuRunTime = runTime;
 				
-				System.out.println("Find burst when choosing ready process to run " + rand);
+//				System.out.println("Find burst when choosing ready process to run " + rand);
 				
 				// Check for ready cycles during every cycle.
 				for (Process subProcess : processList) {
@@ -222,7 +439,6 @@ public class Scheduling {
 				}
 				
 				if (process.cpuTime <= 0) {
-					System.out.println("TERMINATED!");
 					// Time has expired, set runtime to zero and state to terminated
 					process.state = "terminated";
 					process.cpuRunTime = 0;
@@ -237,12 +453,15 @@ public class Scheduling {
 				
 				while (process.state.equals("blocked")) {
 					int nextRand = randomScanner.nextInt();
-					System.out.println("Find I/O burst when blocking a process " + nextRand);
+//					System.out.println("Find I/O burst when blocking a process " + nextRand);
 					
 					// Check for ready cycles during every cycle.
 					for (Process subProcess : processList) {
-						if (subProcess.state.equals("ready"))
+						if (subProcess.state.equals("ready")) {
+							System.out.println(subProcess.index + "\t" + subProcess.waitingTime);
 							subProcess.waitingTime++;
+						}
+							
 						
 						if (subProcess.state.equals("unstarted") && subProcess.arrivalTime >= Process.cycle)
 							subProcess.state = "ready";
@@ -268,6 +487,7 @@ public class Scheduling {
 			process.finishingTime = Process.cycle;
 		} // end for process loop
 		
+		printInfo(processList);
 		randomScanner.close();
 	}
 	
